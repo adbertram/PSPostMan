0dedb1e7c6ed9c12aa956142834a1d9520f42e1a
Adam Bertram <adbertram@gmail.com>
2017-05-22 15:50:50 -0500

    Build logging

--- buildscripts/install.ps1
+++ buildscripts/install.ps1
@@ -1,5 +1,3 @@
-Write-Host 'Installing necessary PowerShell modules...'
-
 $provParams = @{
 	Name = 'NuGet'
 	MinimumVersion = '2.8.5.208'
@@ -11,7 +9,7 @@ $null = Import-PackageProvider @provParams
 
 $requiredModules = @('Pester','PowerShellGet','PSScriptAnalyzer')
 foreach ($m in $requiredModules) {
-	Write-Verbose -Message "Installing module [$($m)]..."
+	Write-Host -Message "Installing module [$($m)]..."
 	Install-Module -Name $m -Force -Confirm:$false
 	Remove-Module -Name $m -Force -ErrorAction Ignore
 	Import-Module -Name $m
